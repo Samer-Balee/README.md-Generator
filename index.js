3a0303a2f7f47efc9f0567182b1d3ecd5717aa80
Samer Balee <samerbalee@gmail.com>
2022-02-03 09:49:06 +1100

    Instaling packages and adding questions

--- index.js
+++ index.js
@@ -1,7 +1,59 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter your project title: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a short project description: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter the installation Instructions: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Enter the usage information: ",
+        name: "usage"
+
+    },
+    {
+        type: "input",
+        message: "Enter contribution guidelines: ",
+        name: "contribute"
+
+    },
+    {
+        type: "list",
+        message: "Choose a license for the project: ",
+        name: "licenses",
+        Choices: ['' , '' , ''],
+    },
+    {
+        type: "input",
+        message: "Enter your Github username: ",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter your Email address: ",
+        name: "username"
+    }, 
+    {
+        type: "input",
+        message: "Enter any tests you are running for your project: ",
+        name: "tests"
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
